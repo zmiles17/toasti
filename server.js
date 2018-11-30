499cbb9226e5f0ee9f03c293f9b916621ab0f4d1
MoNsTeR <zgm_striker17@hotmail.com>
2018-11-29 21:11:36 -0500

    Added test files and server code

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 const express = require("express");
 const app = express();
+const db = require("./models");
+const server = require("http").createServer(app);
+const path = require("path");
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+require("./routes/api-routes")(app);
+
+db.sequelize.sync({ force: true }).then(function() {
+    server.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
