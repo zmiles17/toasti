adf296489062fb716d6edbd28ed648bc18e53d51
Chris Graves <christiangraves93@gmail.com>
2018-12-01 11:35:19 -0500

    added some tests functions

---
+++ public/js/test.js
@@ -0,0 +1,9 @@
+describe('average', function(){
+    it('should return the average of ratings for recipe', function(){
+        expect (average(4,6)).to.equal(5);
+    });
+})
+
+describe('ratingRender', function(){
+    it('should ')
+})
---
+++ routes/test.js
@@ -0,0 +1,11 @@
+describe('', function(){
+    this.beforeEach(function(){
+        didRate = false;
+    })
+
+    it('should change didRate to true on first click and record users rating and increment total ratings', function(){
+        $('#stars').trigger('click');
+        expect($('#'))
+    })
+
+})
