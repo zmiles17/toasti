c4a8c17dce34c65c7020ea4acc990873b750492a
zmiles17 <zgm_striker17@hotmail.com>
2018-12-03 18:16:21 -0500

    Reduced lines of code for transition function

--- public/js/app.js
+++ public/js/app.js
@@ -4,45 +4,15 @@
 
 //search function should not 
 
-function transition1 () {
-    $(".image-container").css("background-image", "url(../assets/cocktail.jpg)");
-}
 
-function transition2 () {
-    $(".image-container").css("background-image", "url(../assets/cocktail2.jpg)");
-}
-
-function transition3 () {
-    $(".image-container").css("background-image", "url(../assets/cocktail3.jpg)");
-}
-
-function transition4 () {
-    $(".image-container").css("background-image", "url(../assets/cocktail4.jpg)");
-}
-
-function transition5 () {
-    $(".image-container").css("background-image", "url(../assets/cocktail5.jpg)");
-}
-
-function transition6 () {
-    $(".image-container").css("background-image", "url(../assets/cocktail6.jpg)");
-}
-
-function transition7 () {
-    $(".image-container").css("background-image", "url(../assets/cocktail7.jpg)");
-}
+let imageIndex = 0;
+const numImages = 7;
 
 function transition () {
-    setTimeout(transition1, 5000);
-    setTimeout(transition2, 10000);
-    setTimeout(transition3, 15000);
-    setTimeout(transition4, 20000);
-    setTimeout(transition5, 25000);
-    setTimeout(transition6, 30000);
-    setTimeout(transition7, 35000);
-};
+    $(".image-container").css("background-image", `url(../assets/cocktail${imageIndex+1}.jpg)`);
+    imageIndex++;
+    if((imageIndex+1)>numImages) imageIndex = 0;
+}
 
-transition();
-
-setInterval(transition, 39100);
+setInterval(transition, 5000);
 
