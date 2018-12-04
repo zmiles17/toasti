80c24a05073a3c5de6387b958c9b13efd97948ca
Veronica Blaha <veronica.blaha@gmail.com>
2018-12-04 15:40:51 -0500

    created working search function and displaying search results on search-results.html

--- public/js/search.js
+++ public/js/search.js
@@ -4,3 +4,33 @@
 //return the results : a list of json objects back- the name, the instructions, the ingredients, the rating
 //order the recipes by rating
 //render the results 
+$('document').ready(function () {
+  const search = function () {
+    const searchterm = $('.search-bar').val();
+     fetch(`/api/recipe?q=${searchterm}`).then(function(res) {
+       return res.json();
+     }).then(function (data) {
+      const el = $('.search-results');
+      el.empty();
+      data.forEach(function (recipe,index) {
+        const recipeEl = $('<div class="recipe">');
+        recipeEl.append(`<h3 class="recipe-title">${recipe.name}</h3>`);
+        recipeEl.append(`<p class="instruction-body">${recipe.instruction}</p>`);
+
+        const ingredientsListEl = $(`<ul class="ingredient-list">`);
+        recipe.ingredients.forEach(function (ingredient) {
+          ingredientsListEl.append(`<li>${ingredient.name}</li>`);
+        });
+        recipeEl.append(ingredientsListEl)
+
+        el.append(recipeEl);
+      })
+    })
+  }
+  $('.search-bar').keypress(function (e) {
+    if (e.which == 13) {//Enter key pressed
+      e.preventDefault();
+      search();//Trigger search button click event
+    }
+  });
+});
