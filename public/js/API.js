6f3a6e6b4a7eac898f7bb324c12e0e4f551572a9
Veronica Blaha <veronica.blaha@gmail.com>
2018-12-07 19:04:30 -0500

    committing to view CHristian's files

---
+++ public/js/API.js
@@ -0,0 +1,12 @@
+// const app = document.getElementById('');
+
+// const request = new XMLHttpRequest();
+// request.open('GET', 'web address', true);
+// request.onload = function () {
+//   const data = JSON.parse(this.response);
+//   if (request.status >= 200 && request.status < 400) {
+//     data.forEach(recipe => {
+//       const 
+//     })
+//   }
+// }
--- schema.sql
+++ schema.sql
@@ -45,5 +45,22 @@ INSERT INTO recipedb.recipes (`id`,`name`,`instruction`,`createdAt`,`updatedAt`)
 INSERT INTO recipedb.ingredients (`id`,`name`,`createdAt`,`updatedAt`,`recipeId`) VALUES (DEFAULT,'2 oz Jack Daniels Tennessee Whiskey',NOW(),NOW(),5);
 INSERT INTO recipedb.ingredients (`id`,`name`,`createdAt`,`updatedAt`,`recipeId`) VALUES (DEFAULT,'5 oz Coca Cola',NOW(),NOW(),5);
 
+-- moscow mule
+INSERT INTO recipedb.recipes (`id`,`name`,`instruction`,`createdAt`,`updatedAt`) VALUES (6,'Moscow Mule',' Combine vodka and ginger beer in a copper mug or highball glass filled with ice. Add lime juice. Stir gently and garnish with a lime slice.',NOW(),NOW());
+INSERT INTO recipedb.ingredients (`id`,`name`,`createdAt`,`updatedAt`,`recipeId`) VALUES (DEFAULT,'4 oz Ginger beer',NOW(),NOW(),6);
+INSERT INTO recipedb.ingredients (`id`,`name`,`createdAt`,`updatedAt`,`recipeId`) VALUES (DEFAULT,'1 1/2 oz Vodka',NOW(),NOW(),6);
+INSERT INTO recipedb.ingredients (`id`,`name`,`createdAt`,`updatedAt`,`recipeId`) VALUES (DEFAULT,'1/6 oz Lime juice',NOW(),NOW(),6);
 
+-- gimlet
+INSERT INTO recipedb.recipes (`id`,`name`,`instruction`,`createdAt`,`updatedAt`) VALUES (7,'Gimlet',' Mix and serve. Garnish with a slice of lime.',NOW(),NOW());
+INSERT INTO recipedb.ingredients (`id`,`name`,`createdAt`,`updatedAt`,`recipeId`) VALUES (DEFAULT,'One part sweetened Lime Juice',NOW(),NOW(),7);
+INSERT INTO recipedb.ingredients (`id`,`name`,`createdAt`,`updatedAt`,`recipeId`) VALUES (DEFAULT,'Four parts Gin',NOW(),NOW(),7);
 
+--french 
+INSERT INTO recipedb.recipes (`id`,`name`,`instruction`,`createdAt`,`updatedAt`) VALUES (8,'French 75','Using zester or paring knife, slice peel from lemon in long, thin spiral. Reserve lemon for another use and set peel aside.
+In cocktail shaker, combine gin, lemon juice, and simple syrup. Add ice and shake vigorously for 20 seconds. Strain into chilled Champagne flute and top with sparkling wine.
+Curl lemon peel around finger to create twist at least 6 inches long. Garnish drink with twist and serve immediately.',NOW(),NOW());
+INSERT INTO recipedb.ingredients (`id`,`name`,`createdAt`,`updatedAt`,`recipeId`) VALUES (DEFAULT,'1 1/2 ounces gin',NOW(),NOW(),8);
+INSERT INTO recipedb.ingredients (`id`,`name`,`createdAt`,`updatedAt`,`recipeId`) VALUES (DEFAULT,'3/4 ounce fresh lemon juice',NOW(),NOW(),8);
+INSERT INTO recipedb.ingredients (`id`,`name`,`createdAt`,`updatedAt`,`recipeId`) VALUES (DEFAULT,'2 ounces dry sparkling wine, such as brut Champagne, chilled',NOW(),NOW(),8);
+INSERT INTO recipedb.ingredients (`id`,`name`,`createdAt`,`updatedAt`,`recipeId`) VALUES (DEFAULT,'1 lemon',NOW(),NOW(),8);
